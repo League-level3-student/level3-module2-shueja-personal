3b86b9f15203540e9c3d58c2073496aec1e9c4dd
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-01-16 12:42:28 -0800

    created searching algorithms

--- src/_01_Searching_Algorithms/_02_InterpolationSearch.java
+++ src/_01_Searching_Algorithms/_02_InterpolationSearch.java
@@ -15,12 +15,24 @@ public class _02_InterpolationSearch {
         //2. create two integers called start and end.
 		//   initialize them to 0 and the length of the array minus one
 		//   respectively
-      
+      int start = 0;
+      int end  = array.length -1;
         //3. Make a while loop that runs while the following conditions are true:
         //   start is less than or equal to end
         //   value is greater than or equal to the array element at start
         //   value is less than or equal to the array element at end
-
+      while (start <= end && value >= array[start] && value >= array[end]) {
+    	  int position = start + (((end - start) / (array[end] - array[start])) * (value - array[start]));
+    	  if(array[position] == value) {
+    		  return position;
+    	  }
+    	  else if(array[position] < value) {
+    		  start = position-1;
+    	  }
+    	  else {
+    		  end = position-1;  
+    	  }
+      }
 			//4. create an integer called position
         	//  initialize it to the following:
         	//  start + (((end - start) / (array[end] - array[start])) * (value - array[start]));
@@ -35,6 +47,6 @@ public class _02_InterpolationSearch {
             //7. otherwise, set end equal to position minus one
           
         //8. Return -1 because the value was not found
-        return 0;
+        return -1;
     }
 }
